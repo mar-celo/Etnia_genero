67df147cb207ddb90d823cf76e465aace515d434
mar-celo <mrodriguesribeiro@gmail.com>
2024-04-19 09:25:58 -0300

    workflow alterado

---
+++ data.R
@@ -0,0 +1,134 @@
+
+
+## code to prepare `DATASET` dataset goes here
+install.packages(c("renv", "dplyr", "lubridate", "janitor", "readr", "echarts4r", "htmltools", "stringr" ))
+
+pacotes <- renv::dependencies() |>
+  dplyr::filter(!Package %in% c("renv", "dplyr")) |>
+  dplyr::pull(Package) |>
+  unique()
+
+install.packages(pacotes)
+
+library(janitor)
+library(dplyr)
+library(readr)
+library(echarts4r)
+library(htmltools)
+
+
+# Carregue a biblioteca lubridate
+library(lubridate)
+
+# Obtenha a data atual
+data_atual <- Sys.Date()
+
+
+# Subtrai um mês da data atual
+data_mes_anterior <- data_atual %m-% months(1)
+
+# Formate o mês como três letras iniciais com a primeira letra em maiúscula
+mes_anterior_abreviado <- format(data_mes_anterior, "%B de %Y") |> stringr::str_to_title()
+
+
+
+# Carregar base de dados direto do PEP
+
+# df <- readr::read_delim("Y:/PEP/PEP_reload/PEP_qvd_InOutrasFontes/Fontes_CSV/Infograficos/etnia_raca.csv", 
+#     delim = ";", escape_double = FALSE, trim_ws = TRUE)
+
+# Carregar base de dados direto do repositorio
+
+#df <- readr::read_csv("data/etnia_raca.csv")
+
+# Conferencia com o PEP
+
+# df_conf <- df |>
+#   filter(`Agrupamento Geral` == 'CCE & FCE',
+#          `Mês Cargos` == mes_anterior_abreviado) |> dplyr::summarise(
+#            total = sum(`Quantidade de Vinculos (Cargos e Funções)`)
+#          ) 
+#   group_by(
+#     # orgao_superior_cargos_e_funcoes,
+#     # orgao_vinculado_cargos_e_funcoes,
+#     `Nome Cor Origem Etnica` ,
+#     # sexo,
+#     `Decreto Nivel`
+#   ) |>
+#   dplyr::summarise(
+#     total = sum(`Quantidade de Vinculos (Cargos e Funções)`)
+#   ) |>
+#   ungroup() |>
+#   filter(!`Decreto Nivel` %in% c("Nível 18"))
+                             
+
+# Etnia = Negras(Preta +Pardo) e demais
+
+df <- df |>  
+  filter(`Agrupamento Geral` == 'CCE & FCE', 
+         `Mês Cargos` == mes_anterior_abreviado)  |> 
+  #filter( stringr::str_detect(df$`Nível Função2`, '^(FCE)|(CCE)'))  |>
+  mutate(
+  Etnia = case_when( 
+    `Cor Origem Etnica` %in% c(4, 6)  ~ "Negras",
+    .default = "Demais Raça/Cor"
+  )
+) |> janitor::clean_names()
+
+
+
+
+
+
+# tratamento da base
+
+tabela <- df |> 
+ group_by(
+ orgao_superior_cargos_e_funcoes,
+ orgao_vinculado_cargos_e_funcoes,
+  etnia ,
+  sexo,
+ decreto_nivel
+ ) |>
+  dplyr::summarise(
+    total = sum(quantidade_de_vinculos_cargos_e_funcoes)
+  ) |> 
+  ungroup() |>  
+  filter(!decreto_nivel %in% c("Nível 18")) |> 
+  # janitor::adorn_totals()
+  rename(
+    `Órgão Superior` = orgao_superior_cargos_e_funcoes,
+    `Órgão` = orgao_vinculado_cargos_e_funcoes,
+    `Cargo-Função` = decreto_nivel
+  ) |> 
+  tidyr::pivot_wider(
+    names_from =sexo,
+    values_from = total) |> 
+  rowwise()  |> 
+  mutate(
+    Total = sum(c_across(Fem:Mas), na.rm = TRUE
+    )
+  ) |> 
+  filter(`Cargo-Função` %in% c("Nível 1 a 12", "Nível 13 a 17"))
+
+
+
+# Percentual das funcoes por etnia
+
+tabela <- tabela |>
+  # filter(Orgão == "Advocacia-Geral Da Uniao",
+  #        `Cargo-Função` == "FCPE & FEX"
+  #        ) |> 
+  group_by(`Órgão`,  `Cargo-Função`) |> 
+  mutate(
+    `% Cargo-Função/Etnia` = scales::percent(Total/sum(Total, na.rm = TRUE))
+  ) |>  ungroup()
+
+
+
+# Salvar base tratada
+saveRDS(tabela, "data/Tab.rds")
+
+
+
+
---
+++ pag_incial.R
@@ -0,0 +1,164 @@
+## code to prepare `DATASET` dataset goes here
+install.packages(c("renv", "dplyr", "lubridate", "janitor", "readr",
+                   "echarts4r", "htmltools", "stringr", "crosstalk", "readxl", "RColorBrewer", 
+                   "reactablefmtr", "sysfonts", "showtext", "htmlwidgets" ))
+
+pacotes <- renv::dependencies() |>
+  dplyr::filter(!Package %in% c("renv", "dplyr")) |>
+  dplyr::pull(Package) |>
+  unique()
+
+install.packages(pacotes)
+
+
+
+# Carregar pacotes
+library(DT) 
+library(plotly)
+library(crosstalk)
+library(readxl)
+library(RColorBrewer)
+library(tidyverse)
+library(reactablefmtr)
+library(sysfonts)
+library(showtext)
+library(htmltools)
+library(htmlwidgets)
+
+#Por órgão 
+
+dados <- readRDS("data/Tab.rds") |> 
+  filter(`Órgão` != "Agencia Brasileira De Inteligencia")   |>  
+  dplyr::rename(`Raça/Cor` = etnia, 
+                `%` = `% Cargo-Função/Etnia`)
+
+sd_total <- SharedData$new(dados, group = "funcao")
+
+# Retirar ABIN 
+
+data_funcao <- dados |> select(`Órgão Superior`,
+                               Órgão, `Raça/Cor`, 
+                               `Cargo-Função`, Fem, Mas, Total) |> 
+  tidyr::pivot_wider(names_from = `Cargo-Função`, values_from = Total) |> 
+  filter(Órgão != "Agencia Brasileira De Inteligencia")
+
+
+sd_pizza <- SharedData$new(data_funcao, group = "funcao")
+
+mes <- format(Sys.Date() %m-% months(1), "%B de %Y") |> stringr::str_to_title()
+
+filtro_Orgao_pizza <- filter_select("Orgao_PIZZA", 
+                                    paste0("Selecione um Órgão: ", "(",mes, ")"),
+                                    sd_pizza, 
+                                    allLevels = TRUE,
+                                    ~Órgão,
+                                    multiple = FALSE
+)
+
+pier_Nivel1a12 <- plot_ly() |>  
+  add_pie(
+    data = sd_pizza,
+    labels = ~`Raça/Cor`,
+    values = ~`Nível 1 a 12`,
+    name = "Nível 1 a 12"
+    #domain = list(x = c(0, 0.4), y = c(0, 1))
+  )   
+
+pier_Nivel13a17 <- plot_ly() |>  
+  add_pie(
+    data = sd_pizza,
+    labels = ~`Raça/Cor`,
+    values = ~`Nível 13 a 17`,
+    name = "Nível 13 a 17"
+    #domain = list(x = c(0.6, 1), y = c(0, 1))
+  )   
+
+
+figpier_Nivel1a12 <- pier_Nivel1a12 |>  
+  layout(title = "Nível 1 a 12",
+         plot_bgcolor='#e5ecf6', 
+         xaxis = list( 
+           zerolinecolor = '#ffff', 
+           zerolinewidth = 2, 
+           gridcolor = 'ffff'), 
+         yaxis = list( 
+           zerolinecolor = '#ffff', 
+           zerolinewidth = 2, 
+           gridcolor = 'ffff'),
+         showlegend=TRUE,showlegend2=TRUE, 
+         margin = 0.01) 
+
+figpier_Nivel13a17 <- pier_Nivel13a17 |>  
+  layout(title = "Nível 13 a 17",
+         plot_bgcolor='#e5ecf6', 
+         xaxis = list( 
+           zerolinecolor = '#ffff', 
+           zerolinewidth = 2, 
+           gridcolor = 'ffff'), 
+         yaxis = list( 
+           zerolinecolor = '#ffff', 
+           zerolinewidth = 2, 
+           gridcolor = 'ffff'),
+         showlegend=TRUE,showlegend2=TRUE, 
+         margin = 0.01) 
+
+
+# table
+
+# Definir as configurações da tabela, incluindo o idioma em Português Brasil
+
+
+# Criar a tabela usando datatable com as configurações personalizadas
+configuracao <- list(
+  language = list(
+    infoFiltered = "(filtrado num total de _MAX_ registros)",
+    info = "Exibindo _START_ até _END_ de _TOTAL_ registros",
+    lengthMenu = "Mostrar _MENU_ registros por página",
+    search = "Pesquisar:",
+    paginate = list(
+      first = "Primeiro",
+      last = "Último",
+      `next` = "Próximo",
+      previous = "Anterior"),
+    aria = list(
+      sortAscending = ": ativar para classificar coluna em ordem crescente",
+      sortDescending = ": ativar para classificar coluna em ordem decrescente"
+    )
+  ),
+  dom = 'Bfrtip',
+  buttons = c('csv', 'excel', 'pdf', 'print')
+  
+)
+
+
+dt2 <- sd_total |> 
+  DT::datatable(#filter = "top",
+    #style = "bootstrap",
+    class = "compact",
+    width = "100%",
+    extensions = 'Buttons',
+    options = configuracao
+  )
+
+# gerar para a tela inicial
+
+# Gere o html_object e salve o Viewer como index.html na pasta observatório
+html_object <- bscols(
+  # ranking_1_a_12,
+  # ranking_13_a_17,
+  filtro_Orgao_pizza,
+  figpier_Nivel1a12,
+  figpier_Nivel13a17,
+  widths = c(
+    #6,6,
+    12, 6,6),
+  device = "sm")
+
+# bscols(filtro_Orgao_pizza, widths = 6)
+
+# salvar html_object como index.html
+
+
+library(htmltools)
+
+save_html(html_object, file = "observatorio/index.html")
