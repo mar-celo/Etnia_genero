96658adfd5e49e68b2267c2ceb8915aa81a76506
marcelo <mrodriguesribeiro@gmail.com>
2024-10-21 10:59:13 -0300

    atualizacao setembro/24

--- src/pag_incial.R
+++ src/pag_incial.R
@@ -45,6 +45,7 @@ data_funcao <- dados |> select(`Órgão Superior`,
 
 sd_pizza <- SharedData$new(data_funcao, group = "funcao")
 
+mes <- "Agosto de 2024"
 mes <- format(Sys.Date() %m-% months(1), "%B de %Y") |> stringr::str_to_title()
 
 filtro_Orgao_pizza <- filter_select("Orgao_PIZZA", 
