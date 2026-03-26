6b7b6012755de450b91f1fe46279cfb56ea08081
Wesley De Jesus Silva <wesley.jesus@gestao.gov.br>
2026-03-26 20:18:47 -0300

    razão de vagas necessárias calculado

--- data.R
+++ data.R
@@ -1,7 +1,8 @@
 
 
 ## code to prepare `DATASET` dataset goes here
-install.packages(c("renv","remotes", "dplyr", "lubridate", "janitor", "readr", "echarts4r", "htmltools", "stringr" ))
+install.packages(c("renv","remotes", "dplyr", "lubridate", "janitor", "readr",
+                   "echarts4r", "htmltools", "stringr" ,"zoo"))
 
 pacotes <- renv::dependencies() |>
   dplyr::filter(!Package %in% c("renv", "dplyr")) |>
@@ -16,6 +17,17 @@ library(dplyr)
 library(readr)
 library(echarts4r)
 library(htmltools)
+library(zoo)
+library(stringr)
+
+
+
+limpa_string <- function(x){
+  x %>% 
+    tolower() %>% 
+    iconv("utf-8", "ASCII//TRANSLIT") %>% 
+    str_squish()
+}
 
 
 # Carregue a biblioteca lubridate
@@ -36,13 +48,17 @@ data_mes_anterior <- data_atual %m-% months(1)
 
 # Formate o mês como três letras iniciais com a primeira letra em maiúscula
 mes_anterior_abreviado <- format(data_mes_anterior, "%b") |> stringr::str_to_title()
-
+ano_corrente <- year(data_mes_anterior)
 
 
 # Carregar base de dados direto do PEP
+# pep_folder <- "Y:/PEP/PEP_reload/PEP_qvd_InOutrasFontes/Infograficos/
+pep_folder <- "C:/Users/wesley.jesus/Documents/Monitoramento_politicas_SGP/monitoramento_cotas"
 
-df <- readr::read_delim("Y:/PEP/PEP_reload/PEP_qvd_InOutrasFontes/Infograficos/etnia_raca.csv",
-     delim = ";", escape_double = FALSE, trim_ws = TRUE)
+df <- readr::read_delim(file.path(pep_folder, "/etnia_raca.csv"),
+                        delim = ";", escape_double = FALSE, trim_ws = TRUE)
+
+
 
 # Carregar base de dados direto do repositorio
 
@@ -50,9 +66,11 @@ df <- readr::read_delim("Y:/PEP/PEP_reload/PEP_qvd_InOutrasFontes/Infograficos/e
 
 # Conferencia com o PEP
 
-df_conf <- df |>
-  filter(`Agrupamento Geral` == 'CCE & FCE',
-         `Mês Cargos` == mes_anterior_abreviado) |>
+df_conf <- etnia_raca |>
+  # filter(#`Agrupamento Geral` == 'CCE & FCE' ,
+  #        `Mês Cargos` == mes_anterior_abreviado,
+  #         `Ano Cargos` == ano_corrente
+  #         ) |>
   # dplyr::summarise(
   #          total = sum(`Quantidade de Vinculos (Cargos e Funções)`)
   #        ) |> 
@@ -61,20 +79,20 @@ df_conf <- df |>
     # orgao_vinculado_cargos_e_funcoes,
     # `Nome Cor Origem Etnica`,
     # sexo,
-    `Decreto Nivel`
+    `Mês-Ano Cargos`
+    #`Decreto Nivel`
   ) |>
   dplyr::summarise(
     total = sum(`Quantidade de Vinculos (Cargos e Funções)`)
   ) |>
-  ungroup() |>
-  filter(!`Decreto Nivel` %in% c("Nível 18"))
+  ungroup() %>%
+  mutate(ano_mes := as.yearmon(`Mês-Ano Cargos`, "%B %Y")) #|>
+  # filter(!`Decreto Nivel` %in% c("Nível 18"))
                              
 
 # Etnia = Negras(Preta +Pardo) e demais
 
-df <- df |>  
-  filter(`Agrupamento Geral` == 'CCE & FCE', 
-         `Mês Cargos` == mes_anterior_abreviado)  |> 
+df <- df  |> 
   #filter( stringr::str_detect(df$`Nível Função2`, '^(FCE)|(CCE)'))  |>
   mutate(
   Etnia = case_when( 
@@ -90,17 +108,20 @@ df <- df |>
 
 # tratamento da base
 
-tabela <- df |> 
- group_by(
- orgao_superior_cargos_e_funcoes,
- orgao_vinculado_cargos_e_funcoes,
-  etnia ,
-  sexo,
- decreto_nivel
- ) |>
+tabela <- df |>  
+  filter(agrupamento_geral == 'CCE & FCE', 
+         ano_cargos == 2026,
+         mes_cargos == "Jan") |> 
+  group_by(
+    orgao_superior_cargos_e_funcoes,
+    orgao_vinculado_cargos_e_funcoes,
+    etnia ,
+    sexo,
+    decreto_nivel
+    ) |>
   dplyr::summarise(
     total = sum(quantidade_de_vinculos_cargos_e_funcoes)
-  ) |> 
+    ) |> 
   ungroup() |>  
   filter(!decreto_nivel %in% c("Nível 18")) |> 
   # janitor::adorn_totals()
@@ -108,11 +129,11 @@ tabela <- df |>
     `Órgão Superior` = orgao_superior_cargos_e_funcoes,
     `Órgão` = orgao_vinculado_cargos_e_funcoes,
     `Cargo-Função` = decreto_nivel
-  ) |> 
+  ) %>% #View
   tidyr::pivot_wider(
     names_from =sexo,
     values_from = total) |> 
-  rowwise()  |> 
+  rowwise()  #%>% View
   mutate(
     Total = sum(c_across(Fem:Mas), na.rm = TRUE
     )
@@ -139,4 +160,108 @@ saveRDS(tabela, "data/Tab.rds")
 
 
 
+### Indicadores 1 e 2: % de negros (mensal) ----
 
+
+### Indicador 3: necessidade de cargos vagos ----
+
+cargos_disp <- readxl::read_excel("data/orgao_superior_cargos.xlsx") %>% setDT
+cargos_disp[,`:=`(
+  
+  ## limpeza no nome do órgão
+  orgao = limpa_string(`Órgão ou Entidade`),
+  
+  ## filtrando para níveis 1 a 12, 13 a 17 
+  `Cargo-Função` =
+    ifelse(
+      as.numeric(`Nível do Cargo/Função`) %in% 1:12,
+      "Nível 1 a 12",
+      ifelse(
+        as.numeric(`Nível do Cargo/Função`) %in% 13:17,
+        "Nível 13 a 17",
+        NA)
+    )
+)]
+
+
+# total de cargos FCE e CCE vagos por órgão e nível
+cargos_disp[!is.na(`Cargo-Função`),
+            .(CCE = sum(as.numeric(CCE),na.rm = T),
+              FCE = sum(as.numeric(FCE),na.rm = T)
+            ),
+            .(`Órgão ou Entidade`,orgao,`Cargo-Função`)] %>% 
+  rowwise() %>% 
+  mutate(
+    total_dist = sum(c_across(CCE:FCE), na.rm = TRUE)
+    ) %>% 
+  janitor::clean_names()%>%
+  setDT -> cargos_niveis_disp
+
+
+# tratamento da base
+tabela_vagos <- df |>  
+  filter(agrupamento_geral == 'CCE & FCE', 
+         ano_cargos == 2026,
+         mes_cargos == "Jan") |> 
+  group_by(
+    orgao_superior_cargos_e_funcoes,
+    orgao_vinculado_cargos_e_funcoes,
+    etnia,
+    decreto_nivel
+  ) %>% 
+  dplyr::summarise(
+    total_negras = sum(quantidade_de_vinculos_cargos_e_funcoes)
+  ) |> 
+  ungroup() |>  
+  filter(!decreto_nivel %in% c("Nível 18")) %>%
+  setDT %>%
+  .[,Total_ocupados := sum(total_negras),
+    .(orgao_superior_cargos_e_funcoes,
+      orgao_vinculado_cargos_e_funcoes,
+      decreto_nivel)] %>% 
+  filter(etnia == "Negras") %>%
+  select(-etnia)
+
+# cruzamento com cargos vagos
+tabela_vagos[,`:=`(orgao_sup_clean = limpa_string(orgao_superior_cargos_e_funcoes),
+                   orgao_vinc_clean = limpa_string(orgao_vinculado_cargos_e_funcoes)
+                )]
+
+
+tabela_vagos <-
+  tabela_vagos %>%
+  # left_join(cargos_niveis_disp[,.(orgao,
+  #                                 cargo_funcao,
+  #                                 total_dist)],
+  #           by = c("orgao_sup_clean" = "orgao",
+  #                  "decreto_nivel" = "cargo_funcao")) %>%
+  left_join(cargos_niveis_disp[,.(orgao,
+                                  cargo_funcao,
+                                  total_dist)],
+            by = c("orgao_vinc_clean" = "orgao",
+                   "decreto_nivel" = "cargo_funcao")) %>%
+  # .[,total_dist := ifelse(is.na(total_dist.x),total_dist.y,total_dist.x)] %>% 
+  select(-orgao_sup_clean,-orgao_vinc_clean)#,
+         # -total_dist.x,-total_dist.y) 
+
+tabela_vagos[,`:=`( necessidade_vagas =  ceiling(Total_ocupados*0.3) - total_negras,
+                    cargos_disponiveis = total_dist - Total_ocupados)]
+tabela_vagos[,`:=`( indice_necessidade = (necessidade_vagas/(cargos_disponiveis + 1)) %>%
+                      ifelse(.< 0,NA,.))]
+
+tabela_vagos <- 
+  select(tabela_vagos,
+         orgao_superior_cargos_e_funcoes,
+         orgao_vinculado_cargos_e_funcoes,
+         decreto_nivel,
+         Total_ocupados,
+         total_negras,
+         # total_dist,
+         necessidade_vagas,
+         cargos_disponiveis,
+         indice_necessidade
+  ) %>%
+  filter(!is.na(indice_necessidade))
+
+# Salvar base tratada
+saveRDS(tabela_vagos, "data/Tab_ind3.rds")
